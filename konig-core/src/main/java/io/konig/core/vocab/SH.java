74f6f919f0173ceaa9a59d5f9ac93321e378ba2a
Greg McFall <gregory.mcfall@gmail.com>
2015-12-31 10:21:23 -0500

    Added sh vocabulary

---
+++ konig-core/src/main/java/io/konig/core/vocab/SH.java
@@ -0,0 +1,12 @@
+package io.konig.core.vocab;
+
+import org.openrdf.model.URI;
+import org.openrdf.model.impl.URIImpl;
+
+public class SH {
+
+	public static final URI IRI = new URIImpl("http://www.w3.org/ns/shacl#IRI");
+	public static final URI BlankNode = new URIImpl("http://www.w3.org/ns/shacl#BlankNode");
+	public static final URI Literal = new URIImpl("http://www.w3.org/ns/shacl#Literal");
+
+}
