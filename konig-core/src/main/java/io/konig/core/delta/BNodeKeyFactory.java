3092d2fc1e8927bd458adc703016fb46c5575837
Greg McFall <gregory.mcfall@gmail.com>
2016-10-30 12:27:29 -0400

    Support for change set feature.

--- konig-core/src/main/java/io/konig/core/delta/BNodeKeyFactory.java
+++ konig-core/src/main/java/io/konig/core/delta/BNodeKeyFactory.java
@@ -1,5 +1,26 @@
 package io.konig.core.delta;
 
+/*
+ * #%L
+ * konig-core
+ * %%
+ * Copyright (C) 2015 - 2016 Gregory McFall
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+
 import org.openrdf.model.URI;
 
 import io.konig.core.Vertex;
@@ -19,12 +40,5 @@ public interface BNodeKeyFactory {
 	 */
 	BNodeKey createKey(URI predicate, Vertex object);
 	
-	/**
-	 * Determine whether a given predicate is part of the key for BNodes handled by this factory.
-	 * @param predicate The predicate to be tested
-	 * @return True if the supplied predicate is part of the key BNodes handled by this factory, and
-	 * false otherwise.
-	 */
-	boolean isKeyPart(URI predicate);
 
 }
