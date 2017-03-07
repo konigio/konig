ffc733763bd620e6cd79b163795684fc2fb7d331
192040 <192040@PC335646.cts.com>
2017-03-07 18:13:44 +0530

    Partial Fix for #147

--- konig-deploy/src/main/java/io/konig/deploy/ProjectDeployer.java
+++ konig-deploy/src/main/java/io/konig/deploy/ProjectDeployer.java
@@ -1,33 +1,92 @@
 package io.konig.deploy;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.Collections;
+
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.JsonParser;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.services.cloudresourcemanager.CloudResourceManager;
+import com.google.api.services.cloudresourcemanager.model.ListProjectsResponse;
+import com.google.api.services.cloudresourcemanager.model.Project;
+
 public class ProjectDeployer {
 
 	public static void main(String args[]) {
 		
 //		do arguments validation
-		String name = "sample-pearson";
+		String jsonFile = "D:/Tmp/konig.json";
 		String action = "create";
 		
-//		try {
-			
-//			GoogleCredential credential = GoogleCredential.getApplicationDefault(null, null);
-//			GoogleCredential credential = new GoogleCredential().setAccessToken(null);
-//			GoogleCredential credential = new GoogleCredential.Builder().setServiceAccountUser("senthil.renganathan@pearson.com").build();
+		try {
+			    FileReader jsonFileReader = new FileReader(jsonFile);
+			    BufferedReader jsonBufReader = new BufferedReader(jsonFileReader);
+			    JsonFactory jsonFactory = JacksonFactory.getDefaultInstance();
+			    JsonParser jsonParser = jsonFactory.createJsonParser(jsonBufReader);
+			    Project project = jsonParser.parseAndClose(Project.class);
+			    System.out.println(project);
+			    jsonBufReader.close();
+			    jsonFileReader.close();
+		    
+			    HttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
+				GoogleCredential credential = GoogleCredential.getApplicationDefault();
 
-//			Project project = new Project();
-//			project.setName(name);
-//			HttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
-//			JsonFactory jsonFactory = JacksonFactory.getDefaultInstance();
-		
-		
-//	    	CloudResourceManager cloudresourcemanagerService = new CloudResourceManager.Builder(httpTransport,jsonFactory, null).build();
-	    
-//		} catch (GeneralSecurityException e) {
-			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		} catch (IOException e) {
-			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
+			    if (credential.createScopedRequired()) {
+			    	credential = credential.createScoped(Collections.singletonList("https://www.googleapis.com/auth/cloud-platform"));
+			    	}
+
+			    CloudResourceManager cloudresourcemanagerService = new CloudResourceManager.Builder(httpTransport, jsonFactory, credential)
+		  	            																						.setApplicationName("Google Cloud Platform Pearson")
+		  	            																						.build();
+/*
+		  	    CloudResourceManager.Projects.Create requestCreate = cloudresourcemanagerService.projects().create(project);
+		  	    Project responseCreate = requestCreate.execute();
+		  	    System.out.println(responseCreate.getProjectNumber());
+		  	    System.out.println(responseCreate.getProjectId());
+		  	    System.out.println(responseCreate.getLifecycleState());
+		  	    System.out.println(responseCreate.getName());
+		  	    System.out.println(responseCreate.getLabels());
+		  	    System.out.println(responseCreate.getCreateTime());
+*/
+		  	    CloudResourceManager.Projects.List requestList = cloudresourcemanagerService.projects().list();
+		  	    ListProjectsResponse responseList;
+		  	    do {
+		  	    		responseList = requestList.execute();
+		  	    		if (null == responseList.getProjects()) continue;
+		  	    		for (Project projectList : responseList.getProjects()) {
+		  	    			System.out.println(projectList.getName());
+		  	    			System.out.println(projectList.getProjectId());
+		  	    			System.out.println(projectList.getCreateTime());
+		  	    			System.out.println(projectList.getProjectNumber());
+		  	    			System.out.println(projectList.getLabels());
+		  	    		}
+		  	    		requestList.setPageToken(responseList.getNextPageToken());
+		  	    	} while (null != responseList.getNextPageToken());
+
+//		  	  	Project projectUpdateRequest = new Project();
+//		  	  	String projIdUpdate = projectUpdateRequest.getProjectId();
+/*		  	    
+		  	    String projIdUpdate = project.getProjectId();
+		  	    CloudResourceManager.Projects.Update requestUpdate = cloudresourcemanagerService.projects().update(projIdUpdate, project);
+		  	    Project projectUpdateResponse = requestUpdate.execute();
+		  	    System.out.println(projectUpdateResponse.getName());
+	
+*/
+		  	  	String projIdDelete = project.getProjectId();
+		  	  	CloudResourceManager.Projects.Delete requestDelete = cloudresourcemanagerService.projects().delete(projIdDelete);
+		  	  	requestDelete.execute();
+		  	 
+		  	 
+			} catch (GeneralSecurityException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 	}
-}
