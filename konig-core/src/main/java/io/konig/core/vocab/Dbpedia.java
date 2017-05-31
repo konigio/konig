036b290a67abbde48835c67e803ed676790f6a38
Greg McFall <gregory.mcfall@gmail.com>
2017-05-31 17:30:15 -0400

    Added Dbpedia

---
+++ konig-core/src/main/java/io/konig/core/vocab/Dbpedia.java
@@ -0,0 +1,8 @@
+package io.konig.core.vocab;
+
+public class Dbpedia {
+
+	public static final String RESOURCE_BASE_URI = "http://dbpedia.org/resource/";
+	public static final String CATEGORY_BASE_URI = "http://dbpedia.org/resource/Category:";
+
+}
