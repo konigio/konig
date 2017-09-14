05035793d559f2c737624ffbd20e10b115e1b6ac
Uvashriamm <Uvashriamm@users.noreply.github.com>
2017-09-14 11:11:21 +0530

    Fixes #527

---
+++ konig-data-app-gcp/src/main/java/io/konig/data/app/gae/GaeDataMetricComputationServlet.java
@@ -0,0 +1,58 @@
+package io.konig.data.app.gae;
+
+/*
+ * #%L
+ * Konig Data App for Google Cloud Platform
+ * %%
+ * Copyright (C) 2015 - 2017 Gregory McFall
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+
+import java.io.InputStream;
+
+import javax.servlet.ServletException;
+
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.services.bigquery.Bigquery;
+import com.google.api.services.bigquery.BigqueryScopes;
+
+import io.konig.data.app.common.AppEngineUtil;
+import io.konig.data.app.common.MetricComputationServlet;
+import io.konig.sql.runtime.BigQueryMetricComputationService;
+import io.konig.sql.runtime.ClasspathEntityStructureService;
+
+public class GaeDataMetricComputationServlet extends MetricComputationServlet {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Override
+	protected void createDataComputationService(String slug,String query) throws ServletException {
+		
+		try (InputStream input = getClass().getClassLoader().getResourceAsStream(AppEngineUtil.CREDENTIALS_FILE)) {			
+			GoogleCredential credentials = GoogleCredential.fromStream(input);
+			if (credentials.createScopedRequired()) {
+				credentials = credentials.createScoped(BigqueryScopes.all());
+			}				
+			Bigquery bigQuery = new Bigquery.Builder(new NetHttpTransport(), new JacksonFactory(), credentials).build();
+			ClasspathEntityStructureService structureService = ClasspathEntityStructureService.defaultInstance();
+			new BigQueryMetricComputationService(structureService, bigQuery).executeSql(slug, query);
+		} catch (Exception e) {
+			throw new ServletException(e);
+		}		
+	}
+}
