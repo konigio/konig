775762933b188f9fa9e623a68f74a93b21444720
Greg McFall <gregory.mcfall@gmail.com>
2017-12-01 10:22:51 -0500

    Partial fix for #594

--- konig-gcp-integration-testing/gcp-deploy-test/src/integration-test/java/io/konig/test/GcpDeploymentTest.java
+++ konig-gcp-integration-testing/gcp-deploy-test/src/integration-test/java/io/konig/test/GcpDeploymentTest.java
@@ -11,6 +11,7 @@ import org.apache.maven.shared.invoker.InvocationResult;
 import org.apache.maven.shared.invoker.MavenInvocationException;
 import org.codehaus.groovy.control.CompilationFailedException;
 import org.junit.FixMethodOrder;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.junit.runners.MethodSorters;
@@ -22,24 +23,26 @@ import static org.junit.Assert.assertTrue;
 @Category(IntegrationTest.class)
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class GcpDeploymentTest {
+	
+	private static File mavenHome = null;
 
-	@Test
+	@Ignore
 	public void test1GcpMultiProject() throws Exception {
 		InvocationRequest request = new DefaultInvocationRequest();
 		request.setPomFile(new File("src/integration-test/resources/gcp-multi-project/pom.xml"));
 		request.setGoals(Collections.singletonList("install"));
-		DefaultInvoker invoker = new DefaultInvoker();
+		DefaultInvoker invoker = invoker();
 		InvocationResult result = invoker.execute(request);
 		assertTrue(result.getExitCode() != 0 ? result.getExecutionException().toString() : "Success",
 				result.getExitCode() == 0);
 	}
 
-	@Test
+	@Ignore
 	public void test2DemoProject() throws MavenInvocationException {
 		InvocationRequest request = new DefaultInvocationRequest();
 		request.setPomFile(new File("src/integration-test/resources/demo/demo-parent/pom.xml"));
 		request.setGoals(Collections.singletonList("install"));
-		DefaultInvoker invoker = new DefaultInvoker();
+		DefaultInvoker invoker = invoker();
 		InvocationResult result = invoker.execute(request);
 		assertTrue(result.getExitCode() != 0 ? result.getExecutionException().toString() : "Success",
 				result.getExitCode() == 0);
@@ -58,7 +61,7 @@ public class GcpDeploymentTest {
 		new GroovyShell().evaluate(deploymentScript);
 	}
 
-	@Test
+	@Ignore
 	public void test4Script() throws CompilationFailedException, IOException {
 		File file = new File(
 				"src/integration-test/resources/demo/demo-gcp-model/target/generated/gcp/scripts/tear-down.groovy");
@@ -66,5 +69,27 @@ public class GcpDeploymentTest {
 		GroovyShell shell = new GroovyShell();
 		shell.evaluate(file);
 
+	}
+
+
+	private DefaultInvoker invoker() {
+		DefaultInvoker invoker = new DefaultInvoker();
+		File mavenHome = mavenHome();
+		invoker.setMavenHome(mavenHome);
+		return invoker;
+	}
+
+	private File mavenHome() {
+		if (mavenHome == null) {
+
+			for (String dirname : System.getenv("PATH").split(File.pathSeparator)) {
+				File file = new File(dirname, "mvn");
+				if (file.isFile()) {
+					return mavenHome = file.getParentFile().getParentFile();
+				}
+			}
+			throw new RuntimeException("Maven executable not found.");
+		}
+		return mavenHome;
 	}
 }
