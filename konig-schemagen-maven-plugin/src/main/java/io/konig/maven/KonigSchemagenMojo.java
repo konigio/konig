f272d2d5d0c6abd39138c0924869513159f9ba39
uvashriamm <uvashri.amm@gmail.com>
2018-01-18 16:44:54 +0530

    closes #659

--- konig-schemagen-maven-plugin/src/main/java/io/konig/maven/KonigSchemagenMojo.java
+++ konig-schemagen-maven-plugin/src/main/java/io/konig/maven/KonigSchemagenMojo.java
@@ -156,6 +156,7 @@ import io.konig.schemagen.jsonschema.ShapeToJsonSchemaLinker;
 import io.konig.schemagen.jsonschema.TemplateJsonSchemaNamer;
 import io.konig.schemagen.jsonschema.impl.SmartJsonSchemaTypeMapper;
 import io.konig.schemagen.ocms.OracleCloudResourceGenerator;
+import io.konig.schemagen.ocms.OracleDatabaseWriter;
 import io.konig.schemagen.ocms.OracleTableWriter;
 import io.konig.schemagen.plantuml.PlantumlClassDiagramGenerator;
 import io.konig.schemagen.plantuml.PlantumlGeneratorException;
@@ -722,10 +723,13 @@ public class KonigSchemagenMojo  extends AbstractMojo {
 			Configurator config = configurator();
 			config.configure(oracleManagedCloud);
 			File directory = Configurator.checkNull(oracleManagedCloud.getDirectory());
+			File databaseDir = Configurator.checkNull(oracleManagedCloud.getDatabases());
 			File tablesDir = Configurator.checkNull(oracleManagedCloud.getTables());
 			if(directory != null && tablesDir != null) {
 				OracleCloudResourceGenerator resourceGenerator = new OracleCloudResourceGenerator();
 				SqlTableGenerator sqlgenerator = new SqlTableGenerator();
+				OracleDatabaseWriter oracleDatbase = new OracleDatabaseWriter(databaseDir);
+				resourceGenerator.add(oracleDatbase);
 				OracleTableWriter oracle = new OracleTableWriter(tablesDir, sqlgenerator);
 				resourceGenerator.add(oracle);
 				resourceGenerator.dispatch(shapeManager.listShapes());
