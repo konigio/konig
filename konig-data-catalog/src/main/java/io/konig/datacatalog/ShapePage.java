bfe06fbdc3e3fb50b60fdfcf8fa72ffeec3d91bb
RanjaniGuru <ranjani.gurusubramanian@pearson.com>
2018-05-04 11:31:03 +0530

    Fixes #852

--- konig-data-catalog/src/main/java/io/konig/datacatalog/ShapePage.java
+++ konig-data-catalog/src/main/java/io/konig/datacatalog/ShapePage.java
@@ -46,6 +46,7 @@ import io.konig.core.OwlReasoner;
 import io.konig.core.json.SampleJsonGenerator;
 import io.konig.core.util.IOUtil;
 import io.konig.core.util.StringUtil;
+import io.konig.core.vocab.Konig;
 import io.konig.datasource.DataSource;
 import io.konig.datasource.DatasourceFileLocator;
 import io.konig.datasource.TableDataSource;
@@ -55,7 +56,7 @@ import io.konig.shacl.Shape;
 public class ShapePage {
 	private static final Logger logger = LoggerFactory.getLogger(ShapePage.class);
 	private static final String SHAPE_TEMPLATE = "data-catalog/velocity/shape.vm";
-	private static final String[] DATASOURCE_LIST={"GoogleBigQueryTable","GoogleBigQueryView","GoogleCloudSqlTable","AwsAurora"};
+	private static final URI[] DATASOURCE_LIST={Konig.GoogleBigQueryTable,Konig.GoogleBigQueryView,Konig.GoogleCloudSqlTable,Konig.AwsAuroraTable};
 
 	public void render(ShapeRequest request, PageResponse response) throws DataCatalogException {
 
@@ -114,9 +115,8 @@ public class ShapePage {
 	private DataSource getValidDataSource(List<DataSource> shapeDataSourceList,VelocityContext context) {
 		for(DataSource ds:shapeDataSourceList){			
 			for(URI uri:ds.getType()){
-				String localName=uri.getLocalName();
-				if(Arrays.asList(DATASOURCE_LIST).contains(localName)){
-					context.put("DataSource",localName);
+				if(Arrays.asList(DATASOURCE_LIST).contains(uri)){
+					context.put("DataSource",uri.getLocalName());
 					return ds;
 				}
 			}			
