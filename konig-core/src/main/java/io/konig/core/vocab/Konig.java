dc4c25c61096b618716e40986a3e2ac0f5c1d2d8
RanjaniGuru <ranjani.gurusubramanian@pearson.com>
2018-05-29 14:23:44 +0530

    Fixes #896

--- konig-core/src/main/java/io/konig/core/vocab/Konig.java
+++ konig-core/src/main/java/io/konig/core/vocab/Konig.java
@@ -108,7 +108,6 @@ public class Konig {
 	public static final URI shapeDataSource = new URIImpl(SHAPE_DATA_SOURCE);
 	public static final URI bigQuerySource = new URIImpl(BIG_QUERY_SOURCE);
 	public static final URI primaryKey = new URIImpl("http://www.konig.io/ns/core/primaryKey");
-	public static final URI syntheticKey = new URIImpl("http://www.konig.io/ns/core/syntheticKey");
 	public static final URI uniqueKey = new URIImpl("http://www.konig.io/ns/core/uniqueKey");
 	public static final URI syntheticKey = new URIImpl("http://www.konig.io/ns/core/syntheticKey");
 	// Shape Transform Vocabulary
