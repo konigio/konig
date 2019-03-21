eacd898a6205fe199bfefdff72503fa3546cf653
Greg McFall <gregory.mcfall@gmail.com>
2019-03-21 11:31:17 -0400

    Partial fix for #1296

---
+++ konig-transform-beam/src/main/java/io/konig/transform/beam/BeamTransformGenerator.java
@@ -0,0 +1,22 @@
+package io.konig.transform.beam;
+
+import com.sun.codemodel.JCodeModel;
+
+import io.konig.core.showl.ShowlNodeShape;
+
+public class BeamTransformGenerator {
+
+	public BeamTransformGenerator() {
+		
+	}
+	
+	/**
+	 * Generate the Java code for an Apache Beam transform from the data source to the specified target shape.
+	 * @param model The code model in which the Java source code will be stored
+	 * @param targetShape The target shape to be transformed.
+	 */
+	void generateTransform(JCodeModel model, ShowlNodeShape targetShape) {
+		
+	}
+
+}
