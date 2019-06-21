01e83aaae3f7568dcea18d1adf1171c7b536d4f1
uvashriamm <uvashri.amm@gmail.com>
2019-06-21 14:54:44 +0530

    closes #1490

---
+++ konig-transform-beam/src/test/resources/BeamTransformGeneratorTest/float-mapping/example-persontarget-beam/src/main/java/com/example/beam/etl/shape/PersonTargetShapeBeam.java
@@ -0,0 +1,77 @@
+package com.example.beam.etl.shape;
+
+import java.text.MessageFormat;
+import java.util.UUID;
+import org.apache.beam.sdk.coders.StringUtf8Coder;
+import org.apache.beam.sdk.io.TextIO;
+import org.apache.beam.sdk.io.gcp.bigquery.BigQueryIO;
+import org.apache.beam.sdk.options.Description;
+import org.apache.beam.sdk.options.PipelineOptions;
+import org.apache.beam.sdk.options.PipelineOptionsFactory;
+import org.apache.beam.sdk.options.Validation;
+import org.apache.beam.sdk.transforms.ParDo;
+import org.apache.beam.sdk.transforms.SerializableFunction;
+import org.apache.beam.sdk.values.PCollectionTuple;
+import org.apache.beam.sdk.values.TupleTagList;
+
+public class PersonTargetShapeBeam {
+
+    private static String sourceURI(PersonTargetShapeBeam.Options options) {
+        String envName = options.getEnvironment();
+        return ("gs://example-inbound-${environmentName}".replace("${environmentName}", options.getEnvironment())+"/*");
+    }
+
+    public static void process(PersonTargetShapeBeam.Options options) {
+        org.apache.beam.sdk.Pipeline p = org.apache.beam.sdk.Pipeline.create(options);
+        String sourceURI = sourceURI(options);
+        String pattern = "gs://example-inbound-${environmentName}/invalid/{0}".replace("${environmentName}", options.getEnvironment());
+        PCollectionTuple outputTuple = p.apply(org.apache.beam.sdk.io.FileIO.match().filepattern(sourceURI)).apply(org.apache.beam.sdk.io.FileIO.readMatches()).apply("ReadFiles", ParDo.of(new ReadPersonSourceShapeFn()).withOutputTags(ReadPersonSourceShapeFn.successTag, TupleTagList.of(ReadPersonSourceShapeFn.deadLetterTag)));
+        outputTuple.get(ReadPersonSourceShapeFn.deadLetterTag).setCoder(StringUtf8Coder.of()).apply("writeErrorDocument", org.apache.beam.sdk.io.FileIO.<String, String> writeDynamic().via(TextIO.sink()).by(new SerializableFunction() {
+
+            @Override
+            public Object apply(Object input) {
+                return UUID.randomUUID().toString();
+            }
+        }
+        ).to(MessageFormat.format(pattern, "application/vnd.example.ns.shape.person")).withNumShards(1).withDestinationCoder(StringUtf8Coder.of()).withNaming(key -> org.apache.beam.sdk.io.FileIO.Write.defaultNaming(("file-"+ key), ".csv")));
+        PCollectionTuple outputTuple2 = outputTuple.get(ReadPersonSourceShapeFn.successTag).apply("ToPersonTargetShape", ParDo.of(new ToPersonTargetShapeFn()).withOutputTags(ToPersonTargetShapeFn.successTag, TupleTagList.of(ToPersonTargetShapeFn.deadLetterTag)));
+        outputTuple2 .get(ToPersonTargetShapeFn.successTag).apply("WritePersonTargetShape", BigQueryIO.writeTableRows().to("schema.PersonTarget").withCreateDisposition(BigQueryIO.Write.CreateDisposition.CREATE_NEVER).withWriteDisposition(BigQueryIO.Write.WriteDisposition.WRITE_APPEND));
+        outputTuple2 .get(ToPersonTargetShapeFn.deadLetterTag).setCoder(StringUtf8Coder.of()).apply("writeErrorDocument", org.apache.beam.sdk.io.FileIO.<String, String> writeDynamic().via(TextIO.sink()).by(new SerializableFunction() {
+
+            @Override
+            public Object apply(Object input) {
+                return UUID.randomUUID().toString();
+            }
+        }
+        ).to(MessageFormat.format(pattern, "application/vnd.example.ns.shape.person")).withNumShards(1).withDestinationCoder(StringUtf8Coder.of()).withNaming(key -> org.apache.beam.sdk.io.FileIO.Write.defaultNaming(("file-"+ key), ".txt")));
+        p.run();
+    }
+
+    public static void main(String[] args) {
+        PersonTargetShapeBeam.Options options = PipelineOptionsFactory.fromArgs(args).withValidation().as(PersonTargetShapeBeam.Options.class);
+        process(options);
+    }
+
+    public interface Options
+        extends PipelineOptions
+    {
+
+        @Validation.Required
+        @Description("The name of the environment; typically one of (dev, test, stage, prod)")
+        public String getEnvironment();
+
+        public void setEnvironment(String envName);
+
+        public String getNetwork();
+
+        public void setNetwork(String gcpNetwork);
+
+        public String getSubnetwork();
+
+        public void setSubnetwork(String subnetwork);
+
+        public String getWorkerMachineType();
+
+        public void setWorkerMachineType(String workerMachineType);
+    }
+}
---
+++ konig-transform-beam/src/test/resources/BeamTransformGeneratorTest/float-mapping/example-persontarget-beam/src/main/java/com/example/beam/etl/shape/ToPersonTargetShapeFn.java
@@ -0,0 +1,64 @@
+package com.example.beam.etl.shape;
+
+import com.example.beam.etl.common.ErrorBuilder;
+import org.apache.beam.sdk.transforms.DoFn;
+import org.apache.beam.sdk.transforms.DoFn.ProcessContext;
+import org.apache.beam.sdk.values.TupleTag;
+
+public class ToPersonTargetShapeFn
+    extends DoFn<com.google.api.services.bigquery.model.TableRow, com.google.api.services.bigquery.model.TableRow>
+{
+    public static TupleTag<String> deadLetterTag = new TupleTag<String>();
+    public static TupleTag<com.google.api.services.bigquery.model.TableRow> successTag = new TupleTag<com.google.api.services.bigquery.model.TableRow>();
+
+    @DoFn.ProcessElement
+    public void processElement(ProcessContext c) {
+        try {
+            ErrorBuilder errorBuilder = new ErrorBuilder();
+            com.google.api.services.bigquery.model.TableRow outputRow = new com.google.api.services.bigquery.model.TableRow();
+            com.google.api.services.bigquery.model.TableRow personSourceRow = ((com.google.api.services.bigquery.model.TableRow) c.element());
+            heightInches(personSourceRow, outputRow, errorBuilder);
+            id(personSourceRow, outputRow, errorBuilder);
+            if ((!outputRow.isEmpty())&&errorBuilder.isEmpty()) {
+                c.output(successTag, outputRow);
+            }
+            if (!errorBuilder.isEmpty()) {
+                errorBuilder.addError(outputRow.toString());
+                throw new Exception(errorBuilder.toString());
+            }
+        } catch (final Throwable oops) {
+            c.output(deadLetterTag, oops.getMessage());
+        }
+    }
+
+    private void heightInches(com.google.api.services.bigquery.model.TableRow personSourceRow, com.google.api.services.bigquery.model.TableRow outputRow, ErrorBuilder errorBuilder) {
+        Object person_height = ((personSourceRow == null)?null:personSourceRow.get("person_height"));
+        if (person_height!= null) {
+            outputRow.set("heightInches", person_height);
+        } else {
+            errorBuilder.addError("Cannot set heightInches because {PersonSourceShape}.person_height is null");
+        }
+    }
+
+    private void id(com.google.api.services.bigquery.model.TableRow personSourceRow, com.google.api.services.bigquery.model.TableRow outputRow, ErrorBuilder errorBuilder) {
+        Object person_id = ((personSourceRow == null)?null:personSourceRow.get("person_id"));
+        if (person_id!= null) {
+            outputRow.set("id", concat("http://example.com/person/", person_id));
+        } else {
+            errorBuilder.addError("Cannot set id because {PersonSourceShape}.person_id is null");
+        }
+    }
+
+    private String concat(Object... arg) {
+        for (Object obj: arg) {
+            if (obj == null) {
+                return null;
+            }
+        }
+        StringBuilder builder = new StringBuilder();
+        for (Object obj: arg) {
+            builder.append(obj);
+        }
+        return builder.toString();
+    }
+}
